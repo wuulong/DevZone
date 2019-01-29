4fe84ac45809ac8ff32566de46d6d0c7f80b32f2
wuulong <wuulong@gmail.com>
2019-01-30 01:13:39 +0800

    MbitBot: me_ultrasonic function fix

--- MbitBot/MbitBot.py
+++ MbitBot/MbitBot.py
@@ -75,6 +75,7 @@ def move_motor_port(MPort, usevalue):
         motor(port_start[MPort-1], 0)
         motor(port_start[MPort-1]+1, usevalue)
 
+#micro:bit related
 #ex: set_color((10,0,0),(0,0,0),(0,0,10),(10,10,10))
 def set_color(cled1,cled2,cled3,cled4):
     import neopixel
@@ -93,8 +94,69 @@ def light_sensor():
     pin10.write_digital(1)
     display.on()
     return value
-#unit test
 
+#Mbot related
+def me_ultrasonic_sensor(Jpin):
+
+    pin = pin2
+    if Jpin==1:
+        pin = pin14
+    if Jpin==2:
+        pin = pin16
+    if Jpin==3:
+        pin = pin2
+    if Jpin==4:
+        pin = pin4
+        display.off() #microbit default in display mode
+
+    cnt1=0
+    cnt0=0
+    exit_type=0
+
+    #send pulse
+    pin.write_digital(0)
+    utime.sleep_us(2)
+    pin.write_digital(1)
+    utime.sleep_us(10)
+    pin.write_digital(0)
+
+    #pin.set_pull(pin.PULL_DOWN) #pin.PULL_DOWN, NO_PULL
+
+    time_s = utime.ticks_us()
+
+    while True:
+        time_now = utime.ticks_us()
+        pvalue = pin.read_digital()
+        time_use = utime.ticks_diff(time_now,time_s)
+
+        if pvalue ==1:
+            cnt1+=1
+        else:
+            cnt0+=1
+            if cnt1>0 and cnt0>1 :
+                exit_type=2
+                break
+
+        if 100000 - time_use <0: # 10s
+            #display.show("W")
+            exit_type=1 #timeout
+            break
+
+    if time_use>=30000:
+        distance = 0 #400.0 #cm, timeout when real distance = 0
+    else:
+        distance =  time_use * 5/3/58/2 # d * 5 / 3 / 58 cm, don't know why need last /2
+
+    #d = pins.pulseIn(pin, PulseValue.High, 23000);  // 8 / 340 =
+    #return  d #d * 5 / 3 / 58
+
+    #print("(%i,%i,%i,%i,%2.2f)"%(cnt1,cnt0,exit_type,time_use,distance))
+    #print("(%2.2f)"%(distance))
+
+
+#unit test
+def test_light():
+    print("(%i)" % display.read_light_level())
 def test_music():
     import music
     music.play(music.NYAN)
@@ -112,6 +174,8 @@ def test_motor():
 
 def test_servo():
     servo(1,180)
+
+
 
 def test_all():
     test_pixel()
@@ -142,6 +206,22 @@ def test_stepper():
             sleep(10)
         sleep(100)
 
+# utility
+def test_plot():
+    while True:
+        sleep(20)
+        print(str(accelerometer.get_values()))
+
+def test_ultrasonic():
+    while True:
+        print("(%s)" % str(me_ultrasonic_sensor(1)))
+        sleep(100)
+
+
+
+#while True:
+#    test_light()
+test_plot()
 #sample usage
-i2c_init()
-test_all()
+#i2c_init()
+#test_all()
