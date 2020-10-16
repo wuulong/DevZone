dd351e48aa310a62631ca05dda5cbd70931a3f9d
wuulong <wuulong@gmail.com>
2020-10-16 09:15:58 +0800

    add: pg_scratch rivertree support networkx

--- Codes/pg_scratch.py
+++ Codes/pg_scratch.py
@@ -11,6 +11,8 @@ import psycopg2
 from configparser import ConfigParser
 #import geopandas
 import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
 
 def config(filename='database.ini', section='postgresql'):
     """ generate db parameters from ini file"""
@@ -83,13 +85,20 @@ def get_sql(sql_str):
         if conn is not None:
             conn.close()
 
-def gen_rivertree(rivercode=""):
-    """ generate river tree from db:rivercode to dot format
-    parameter: rivercode or "" for all
-        ex: gen_rivertree("130000")
+def gen_rivertree(filename, rivercode="",id_with_name=False):
+    """ 
+        load river tree from db:rivercode to networkx graph,df, output dot format
+        parameter: 
+            filename: filename for dot
+            rivercode: rivercode or "" for all
+            id_with_name: graph ID with name?
+            
+        ex: gen_rivertree(dot_filename, "130000")
+        return: [G,df]
+    
     """
 
-    
+    G = nx.Graph()
     try:
         params = config()
         conn = psycopg2.connect(**params)  
@@ -128,18 +137,55 @@ def gen_rivertree(rivercode=""):
                 name2 = river_name2[0]
             else:
                 name2 = "Undefined"
-            dot_str = "\"%s_\\n%s\"->\"%s_\\n%s\"" %(cols[i],name1,cols[i+1],name2)
+            dot_str = "\"%s_\\n%s\"->\"%s_\\n%s\"" %(cols[i+1],name2,cols[i],name1)
+            #G.add_nodes_from([cols[i], cols[i+1]])
+            if id_with_name:
+                G.add_node("%s-%s" %(cols[i+1],name2), name=name2)
+                G.add_node("%s-%s" %(cols[i],name1), name=name1)
+                G.add_edges_from([("%s-%s" %(cols[i+1],name2), "%s-%s" %(cols[i],name1))])
+            else:
+                G.add_node(cols[i+1], name=name2)
+                G.add_node(cols[i], name=name1)
+                G.add_edges_from([(cols[i+1], cols[i])])
             if dot_str not in dot_strs:
                 dot_strs[dot_str]=1
                 #print("dot string: %s" %(dot_str))
                 lines.append(dot_str)                     
             
     
-    print("digraph G {\n\t")       
-    lines.sort()
-    print( "\t" + "\n\t".join(lines))
-    print("}\n")      
+    #print("digraph G {\n\t")       
+    #lines.sort()
+    #print( "\t" + "\n\t".join(lines))
+    #print("}\n")     
     
+     
+    fp = open(filename, "w")
+    lines.sort()
+    fp.write("%s%s%s" %("digraph G {\n\t\n","\t" + "\n\t".join(lines),"\n}\n"))
+    fp.close() 
+    return G,df
+
+def output_rivertree(G,rivercode_df):
+    if 0:
+        print(list(G.nodes))
+        print(list(G.edges))
+        print(G.nodes['130000'])
+    if 1:
+        nx.readwrite.gml.write_gml(G,"output/rivertree.gml")
+    if 1:
+        pos = nx.spring_layout(G)
+        colors = range(len(G.edges))
+        options = {
+            "node_color": "#A0CBE2",
+            #"edge_color": colors,
+            "width": 1,
+            #"edge_cmap": "#A0CBE2",
+            "with_labels": True,
+        }
+        nx.draw(G, pos, **options)
+        plt.show()
+
+    #nx.readwrite.nx_shp.write_shp(G, "output/rivertree.shp")
 if __name__ == '__main__':
     #connect_test()
     #sql="select * from public.industry_list"
@@ -147,4 +193,6 @@ if __name__ == '__main__':
     #get_sql(sql)
     
     #gen_rivertree("") # 全部
-    gen_rivertree("143000") #頭前溪
+    [G,df]= gen_rivertree("output/rivertree.dot","",False) 
+    output_rivertree(G,df)
+    
